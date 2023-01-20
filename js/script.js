7c7b9cf1985947325effa0d278927fd104805df3
Nazar Yaremii <n.yaremiy791@gmail.com>
2023-01-20 16:18:15 +0200

    Added section HERO and crated filter

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ if(burger && menu) {
     body.classList.toggle('_lock');
   });
 }
+
+const filter =  document.querySelector('.filter');
+
+if(filter) {
+  const items  = filter.querySelectorAll('.block-filter');
+
+  items.forEach(item => {
+    item.addEventListener('click', (event) => {
+      item.querySelector('.block-filter__dropdown').classList.toggle('_active');
+      item.querySelector('.block-filter__icon').classList.toggle('_active');
+
+      if(event.target.classList.contains('block-filter__item')) {
+        item.querySelector('.block-filter__value').textContent = event.target.textContent;
+      }
+    });
+  });
+}
